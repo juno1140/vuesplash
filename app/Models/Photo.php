fa3ea27a58bda67a93007562392a00c163d91a4c
juno1140 <1bey1140@gmail.com>
2020-12-19 20:01:54 +0900

    first commit

---
+++ app/Models/Photo.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
+
+class Photo extends Model
+{
+    use HasFactory;
+
+    /** プライマリキーの型 */
+    protected $keyType = 'string';
+
+    /** IDの桁数 */
+    const ID_LENGTH = 12;
+
+    /** JSONに含める属性 */
+    protected $appends = [
+        'url',
+    ];
+
+    /** JSONに含める属性 */
+    protected $visible = [
+        'id', 'owner', 'url',
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (! Arr::get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    /**
+     * ランダムなID値をid属性に代入する
+     */
+    private function setId()
+    {
+        $this->attributes['id'] = $this->getRandomId();
+    }
+
+    /**
+     * ランダムなID値を生成する
+     * @return string
+     */
+    private function getRandomId()
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+
+        $id = "";
+
+        for ($i = 0; $i < self::ID_LENGTH; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
+    }
+
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id', 'users');
+    }
+
+    /**
+     * アクセサ - url
+     * @return string
+     */
+    public function getUrlAttribute()
+    {
+        return Storage::cloud()->url($this->attributes['filename']);
+    }
+}
---
+++ tests/Feature/PhotoListApiTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Photo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PhotoListApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function should_正しい構造のJSONを返却する()
+    {
+        Photo::factory()->count(5)->create();
+
+        $response = $this->json('GET', route('photo.index'));
+
+        // 生成した写真データを作成日降順で取得
+        $photos = Photo::with(['owner'])->orderBy('created_at', 'desc')->get();
+
+        $expected_data = $photos->map(function ($photo) {
+            return [
+                'id' => $photo->id,
+                'url' => $photo->url,
+                'owner' => [
+                    'name' => $photo->owner->name,
+                ],
+            ];
+        })->all();
+
+        $response->assertStatus(200)
+            // レスポンスJSONのdata項目に含まれる要素が5つであること
+            ->assertJsonCount(5, 'data')
+            // レスポンスJSONのdata項目が期待値と合致すること
+            ->assertJsonFragment([
+                "data" => $expected_data,
+            ]);
+    }
+}
